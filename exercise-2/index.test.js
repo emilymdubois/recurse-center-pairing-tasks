d224cafc220859805cfd92f298cb871bd7478a41
Emily DuBois <emilymdubois@gmail.com>
2018-07-09 14:20:06 -0400

    wip

--- exercise-2/index.test.js
+++ exercise-2/index.test.js
@@ -9,3 +9,33 @@ describe('lispParser', () => {
     expect(actual).toEqual(parsed);
   });
 });
+
+/*
+Tokenizing - separate typing responsibility from parsing algorithm
+
+"1000 + (3 / (4 - 5))"
+[1000, "+", "(", 3, "/", "(", 4, "-", 5, ")", ")"]
+
+* Splitting is limited
+* Loop through initial array
+
+const OPEN_PAREN = {
+  token: '(',
+  type: 'lParen'
+};
+
+[
+  {
+    token: 1000,
+    type: number
+  },
+  OPEN_PAREN
+]
+
+Follow-up:
+ * Implement tokenizing
+ * Reserve recursion AST traversal
+    * Use loops for same-level operations
+ * Lisp interpreter
+ * Merge sort
+*/
